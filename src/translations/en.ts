157fa62d801c62296665ff362bd78eceeb028c26
Guilherme Monte <gmonte@inf.ufsm.br>
2022-03-11 16:23:54 -0300

    feat(i18n): removed potato string from dictionaries

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -1,5 +1,4 @@
 export default {
-  potato: '{howMany, plural, one {Potato} other {Potatoes}}',
   english: 'english',
   french: 'french',
   'brazilian portuguese': 'brazilian portuguese'
--- src/translations/fr.ts
+++ src/translations/fr.ts
@@ -1,5 +1,4 @@
 export default {
-  potato: '{howMany, plural, one {Patate} other {Pommes de terre}}',
   english: 'anglais',
   french: 'français',
   'brazilian portuguese': 'portugais brésilien'
